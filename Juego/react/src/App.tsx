7ad999b40b0e97a324dc6b0a26b0aa7d4762527a
Gorchon <a01254831@tec.mx>
2024-04-09 19:05:16 -0600

    gameboy fe done

--- Juego/react/src/App.tsx
+++ Juego/react/src/App.tsx
@@ -6,10 +6,49 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div style={{ fontSize: "64px" }}>hola mundo</div>
-    </>
+    <div className="main-container">
+      <div className="gameboy-container">
+        <div className="gameboy-header">Gameboy</div>
+        <div className="layout-game">
+          <div className="container-screen">
+            <div className="screen-layout"></div>
+          </div>
+
+          <div className="button-container">
+            <div className="container-pad">
+              <button className="up-btn"></button>
+              <button className="down-btn"></button>
+              <button className="right-btn"></button>
+              <button className="left-btn"></button>
+            </div>
+            <div className="container-select">
+              <div className="container-select-button">
+                <button className="button-select"></button>
+                <div>select</div>
+              </div>
+              <div className="container-start-button">
+                <button className="button-start"></button>
+                <div>start</div>
+              </div>
+            </div>
+            <div className="container-action">
+              <div className="button-a-container">
+                <button
+                  className="button-a"
+                  onClick={() => console.log("ge")}
+                ></button>
+                <div>B</div>
+              </div>
+
+              <div className="button-b-container">
+                <button className="button-b"></button>
+                <div>A</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default App;
