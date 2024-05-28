a49bf8e5e0d558c49a378c4bfcae532757bf0a57
Gorchon <a01254831@tec.mx>
2024-05-27 20:17:37 -0600

    Dev: yeah

--- userIntegrattioon/ingeration/views/dashboard/components/card.jsx
+++ userIntegrattioon/ingeration/views/dashboard/components/card.jsx
@@ -1,26 +1,23 @@
 /*eslint-disable*/
 import {useNavigate } from 'react-router-dom';
 
-function Card({user}) {
+function Card({ user }) {
     console.log("Rendering Card");
-
-    const handleClick = () => {
-        console.log(`Clicked ${user.id}`);
-        navigate(`/user/${user.id}`); 
-    };
 
     const navigate = useNavigate();
 
+    const handleClick = () => {
+        console.log(`Clicked ${user.id}`);
+        navigate(`/user/${user.id}`);
+    };
 
-
-    const Card = ({user})
     return (
         <div style={{
             width: '200px',  // Increased width
             height: '60px',  // Increased height
             border: 'solid black 2px',
             borderRadius: '5px',
-            backgroundColor: 'df3078',  // Lighter background
+            backgroundColor: '#df3078',  // Correct hex value for background color
             display: 'flex',
             flexDirection: 'column',  // Stack children vertically
             justifyContent: 'center',
--- userIntegrattioon/ingeration/views/users/User.jsx
+++ userIntegrattioon/ingeration/views/users/User.jsx
@@ -1,8 +1,61 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import flor from '../../src/assets/flor.svg'
+import { useState } from "react"
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
+    const { id } = useParams(); // destructure the id from useParams
+
+    const fetchDescription = async () => {
+        console.log("ID from users", id);
+        console.log("Fetching Description");
+        const response = await fetch(`http://localhost:3000/descriptions/${id}`); // correct URL concatenation
+        const data = await response.json();
+        console.log(data);
+    }
+
+    const [form, setForm] = useState({
+        description: "",
+        prescription: "",
+    });
+
+    useEffect(() => {
+        console.log("Fetching Description");
+        fetchDescription();
+    }, [id]); // include id as a dependency to re-fetch when id changes
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: value
+        }));
+    }
+
+    return (
+        <div>
+            <div>User</div>
+            <div>
+                <img src={flor} alt="user" />
+            </div>
+            <div>
+                <p>Description</p>
+                <textarea 
+                    name="description" 
+                    value={form.description} 
+                    onChange={handleInputChange} 
+
+                />
+                <p>Prescription</p>
+                     <textarea 
+                    name="prescription" 
+                    value={form.prescription} 
+                    onChange={handleInputChange} 
+                    
+                />
+            </div>
+        </div>
+    )
 }
 
-export default User
+export default User;
