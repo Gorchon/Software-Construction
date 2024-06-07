4a47ef4c335e3cbb9c301a24a69dcfd43bd7ecda
Gorchon <a01254831@tec.mx>
2024-06-06 21:37:23 -0600

    dev: fetching pres

--- server_start/models/descriptionModel.js
+++ server_start/models/descriptionModel.js
@@ -1,20 +1,19 @@
-const { db} = require('../config/db');
+const { db } = require('../config/db');
 
 const getDescriptionById = async (id) => {
     try {
-        const query = 'SELECT U.name,  d. description FROM users U JOIN description d ON U.id = d.userd_id WHERE U.id = $1;';
+        const query = 'SELECT U.name, d.description, d.prescription FROM users U JOIN description d ON U.id = d.userd_id WHERE U.id = $1;';
         const { rows } = await db.query(query, [id]);
         return rows;
     } catch (err) {
         console.log('Error getting description by id', err);
         throw new Error(err);
     }
-
 }
 
-const createDescription = async ( description, prescription,  userId) => {
+const createDescription = async (description, prescription, userId) => {
     try {
-        const query = " INSERT INTO description (description, prescription, userd_id) VALUES ($1, $2, $3) RETURNING *;";
+        const query = "INSERT INTO description (description, prescription, userd_id) VALUES ($1, $2, $3) RETURNING *;";
         const { rows } = await db.query(query, [description, prescription, userId]);
         return rows[0];
     } catch (err) {
@@ -23,4 +22,4 @@ const createDescription = async ( description, prescription,  userId) => {
     }
 }
 
-module.exports = { getDescriptionById, createDescription};
+module.exports = { getDescriptionById, createDescription };
